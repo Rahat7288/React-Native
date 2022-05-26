b47fd31e679ebb0ed4b79d528b4a7508d0422bcf
Rahat <rahatislamakash@gmail.com>
2022-05-27 01:13:47 +0600

    learning about the view, text and style

--- test/App.js
+++ test/App.js
@@ -4,15 +4,33 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    /**
+     * view help use to display the content 
+     * view can inherit the style property that we can customize
+     * we can create view inside other view
+     */
     <View style={styles.container}>
-      <Text>Welcome here</Text>
-      <StatusBar style="auto" />
-      <Text>Rahat</Text>
+
+      <View style = {styles.header}>
+        <Text style = {styles.boldText}>hello World</Text>
+      </View>
+
+      <View style = {styles.body}>
+        <Text>Rahat Islam Akash</Text>
+        <Text>Rahat Islam Akash</Text>
+        <Text>Rahat Islam Akash</Text>
+        <Text>Rahat Islam Akash</Text>
+
+      </View>
     </View>
 
   
   );
 }
+
+/**
+ * style that we use to customize our views
+ */
 
 const styles = StyleSheet.create({
   container: {
@@ -21,4 +39,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgoundColor: 'Red',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
