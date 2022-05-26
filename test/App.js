57245d876461a15b9cd0a074780fbb65c8b6ad79
Rahat <rahatislamakash@gmail.com>
2022-05-27 00:26:35 +0600

    adding react node setup file for learning

---
+++ test/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome here</Text>
+      <StatusBar style="auto" />
+      <Text>Rahat</Text>
+    </View>
+
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
